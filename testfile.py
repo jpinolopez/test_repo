bb89453ecb7332b5412e79ad4e3350c459fc26a2
Jacqueline Pino <jap6527@educcomp02.ccbb.utexas.edu>
2020-12-04 19:35:36 -0600

    made some changes

--- testfile.py
+++ testfile.py
@@ -1,1 +1,9 @@
 print('this is my test file before I build my website')
+#git init #initialize git respository in your home directory
+#git add testfile.py #replace testfile.py with any file in your home directory you want to upload: 
+#git commit -m "test commit" #this commits the file and adds a message (required)
+#git remote add origin https://github.com/YourGithubUsername/test_repo.git #update with your link
+#git push origin master
+#git add testfile.py
+#git commit -m "made some changes"
+#git push origin master
